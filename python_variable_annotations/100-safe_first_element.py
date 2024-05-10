d2fed424964ccc0e40ab131637933110a230ef00
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-10 08:08:21 +0000

    100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -5,13 +5,13 @@
 import List, Union and TypeVar from typing
 correct duck-typed annotations
 """
-from typing import List, Union, TypeVar
+from typing import List, Union, TypeVar, Sequence
 
 
 T = TypeVar('T')
 
 
-def safe_first_element(lst: List[T]) -> Union[T, None]:
+def safe_first_element(lst: Sequence[T]) -> Union[T, None]:
     """
     function safe_first_element
     parameters
