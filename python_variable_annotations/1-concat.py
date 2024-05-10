1009d7746e251b73af35312ba797718b950ae6a0
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-10 06:16:20 +0000

    0 to 3

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+"""
+Define a function concat
+"""
+
+def concat(str1:str , str2:str):
+    """
+    Concat 2 strings
+
+    parameters:
+    str1
+    str2
+
+    return concatenated string
+    """
+    return str(str1 + str2);
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""Define floor function"""
+def floor(n: float) -> int:
+    """ 
+    floor function
+    parameters
+    n: float
+    returns the floor of the float
+    """
+    return (n // 1)
