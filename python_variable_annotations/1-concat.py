025e2685fe05e0782351a8a2f2240e7532efd7d1
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-10 07:01:50 +0000

    corrected number 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -6,7 +6,7 @@ Define a function concat
 """
 
 
-def concat(str1:str , str2:str):
+def concat(str1:str , str2:str) -> str:
     """
     Concat 2 strings
 
@@ -16,4 +16,4 @@ def concat(str1:str , str2:str):
 
     return concatenated string
     """
-    return str(str1 + str2);
+    return str1 + str2;
