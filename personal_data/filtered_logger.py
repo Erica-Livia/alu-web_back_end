812a1568069cc87e8591ed8211438fae139e1a49
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-20 11:43:59 +0000

    redacting formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -22,10 +22,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """class initialization"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """format"""
         message = super(RedactingFormatter, self).format(record)
         return filter_datum(
                 self.fields, self.REDACTION, message, self.SEPARATOR
