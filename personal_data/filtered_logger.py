70c1ab0b6db4b8488b79a1f5578d5ab9d555c1bb
Erica-Livia <e.ingabire1@alustudent.com>
2024-05-20 11:37:24 +0000

    get_logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -39,15 +39,3 @@ def filter_datum(
 
     pattern = '|'.join(f'(?<={field}=)[^{separator}]+' for field in fields)
     return re.sub(pattern, redaction, message)
-
-def get_logger() -> logging.Logger:
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    return logger
