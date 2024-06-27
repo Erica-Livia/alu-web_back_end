d190ca12ca19f7e365f6fdcae7069eb4dcd31f6d
Erica-Livia <e.ingabire1@alustudent.com>
2024-06-27 23:48:48 +0200

    2-5

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -22,7 +22,6 @@ app.config.from_object(Config)
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
--- i18n/5-app.py
+++ i18n/5-app.py
@@ -25,13 +25,14 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-
 def get_locale():
     """Get locale for your application"""
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
