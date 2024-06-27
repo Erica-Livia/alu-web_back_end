deb0c8ccd3906e65dde33fe5813d1f4dacfb9ff8
Erica-Livia <e.ingabire1@alustudent.com>
2024-06-27 23:42:38 +0200

    2-3

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -18,7 +18,7 @@ class Config:
 app.config.from_object(Config)
 
 
-
+@babel.localeselector
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
