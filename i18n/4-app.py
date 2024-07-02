27b16e02c3d4591348c9af2b0c3fc4a92593ab09
Erica-Livia <e.ingabire1@alustudent.com>
2024-07-02 08:36:15 +0200

    correction

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -24,6 +24,7 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
--- i18n/5-app.py
+++ i18n/5-app.py
@@ -31,6 +31,7 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
